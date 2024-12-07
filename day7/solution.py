b60d495f44403b4b2dba1697f8645d2d6bab8d28
Luca Paolella <luca.paolella@gmail.com>
2024-12-07 08:21:27 +0100

    day 7

---
+++ day7/solution.py
@@ -0,0 +1,35 @@
+from pathlib import Path
+from math import prod
+
+current_dir = Path(__file__).parent
+input_file = Path(current_dir, "input.txt")
+
+lines = input_file.read_text().splitlines()
+tests = {
+    int(res): tuple(map(int, xpr.split()))
+    for res, xpr in (l.split(": ") for l in lines)
+}
+
+
+def eqs(res, xpr, ops, tot=0, pos=0):
+    if pos >= len(xpr) - 1:
+        return tot == res
+
+    return any(eqs(res, xpr, ops, op((tot, xpr[pos + 1])), pos + 1) for op in ops)
+
+
+def part1():
+    return sum(
+        res for res, xpr in tests.items() if eqs(res, xpr, [sum, prod], xpr[0], 0)
+    )
+
+
+def part2():
+    cat = lambda nums: int(str(nums[0]) + str(nums[1]))
+    return sum(
+        res for res, xpr in tests.items() if eqs(res, xpr, [sum, prod, cat], xpr[0], 0)
+    )
+
+
+print(part1())
+print(part2())
